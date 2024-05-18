b87b24f4b1809a24133668f55bb563b5222c5368
Nailu <contact@nailu.fr>
2024-05-18 18:51:23 +0200

    Add safeWithdraw to Ranger.sol w/ tests

--- contracts/Ranger.sol
+++ contracts/Ranger.sol
@@ -15,226 +15,257 @@ import "@uniswap/v3-periphery/contracts/base/LiquidityManagement.sol";
 import "hardhat/console.sol";
 
 contract Ranger is IERC721Receiver {
-    address public constant ARB_WETH =
-        0x82aF49447D8a07e3bd95BD0d56f35241523fBab1;
-    // address public constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-    // address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-    address public constant ARB_USDC =
-        0xaf88d065e77c8cC2239327C5EDb3A432268e5831;
-    // address public constant USDC = 0xaf88d065e77c8cC2239327C5EDb3A432268e5831;
+	address public constant ARB_WETH =
+		0x82aF49447D8a07e3bd95BD0d56f35241523fBab1;
+	// address public constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+	// address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+	address public constant ARB_USDC =
+		0xaf88d065e77c8cC2239327C5EDb3A432268e5831;
+	// address public constant USDC = 0xaf88d065e77c8cC2239327C5EDb3A432268e5831;
 
-    uint24 public constant poolFee = 100;
+	uint24 public constant poolFee = 100;
 
-    address public immutable i_owner;
+	address public immutable i_owner;
 
-    INonfungiblePositionManager public constant nonfungiblePositionManager =
-        INonfungiblePositionManager(0xC36442b4a4522E871399CD717aBDD847Ab11FE88);
+	INonfungiblePositionManager public constant nonfungiblePositionManager =
+		INonfungiblePositionManager(
+			0xC36442b4a4522E871399CD717aBDD847Ab11FE88
+		);
 
-    /// @notice Represents the deposit of an NFT
-    struct Deposit {
-        address owner;
-        uint128 liquidity;
-        address token0;
-        address token1;
-    }
+	/// @notice Represents the deposit of an NFT
+	struct Deposit {
+		address owner;
+		uint128 liquidity;
+		address token0;
+		address token1;
+	}
 
-    /// @dev deposits[tokenId] => Deposit
-    mapping(uint256 => Deposit) public deposits;
+	/// @dev deposits[tokenId] => Deposit
+	mapping(uint256 => Deposit) public deposits;
 
-    event newMint(uint _tokenId, uint128 liquidity, uint amount0, uint amount1);
+	event newMint(
+		uint _tokenId,
+		uint128 liquidity,
+		uint amount0,
+		uint amount1
+	);
 
-    event collect(uint amount0, uint amount1);
+	event collect(uint amount0, uint amount1);
 
-    constructor() {
-        i_owner = msg.sender;
-    }
+	event withdraw(uint amount0, uint amount1);
 
-    // Implementing `onERC721Received` so this contract can receive custody of erc721 tokens
-    function onERC721Received(
-        address operator,
-        address,
-        uint256 tokenId,
-        bytes calldata
-    ) external override returns (bytes4) {
-        // get position information
+	constructor() {
+		i_owner = msg.sender;
+	}
 
-        _createDeposit(operator, tokenId);
+	// Implementing `onERC721Received` so this contract can receive custody of erc721 tokens
+	function onERC721Received(
+		address operator,
+		address,
+		uint256 tokenId,
+		bytes calldata
+	) external override returns (bytes4) {
+		// get position information
 
-        return this.onERC721Received.selector;
-    }
+		_createDeposit(operator, tokenId);
 
-    function _createDeposit(address owner, uint256 tokenId) internal {
-        (
-            ,
-            ,
-            address token0,
-            address token1,
-            ,
-            ,
-            ,
-            uint128 liquidity,
-            ,
-            ,
-            ,
+		return this.onERC721Received.selector;
+	}
 
-        ) = nonfungiblePositionManager.positions(tokenId);
+	function _createDeposit(address owner, uint256 tokenId) internal {
+		(
+			,
+			,
+			address token0,
+			address token1,
+			,
+			,
+			,
+			uint128 liquidity,
+			,
+			,
+			,
 
-        // set the owner and data for position
-        // operator is msg.sender
-        deposits[tokenId] = Deposit({
-            owner: owner,
-            liquidity: liquidity,
-            token0: token0,
-            token1: token1
-        });
-    }
+		) = nonfungiblePositionManager.positions(tokenId);
 
-    function mintNewPosition(
-        uint amount0ToMint,
-        uint amount1ToMint
-    )
-        external
-        onlyOwner
-        returns (uint _tokenId, uint128 liquidity, uint amount0, uint amount1)
-    {
-        // Approve the position manager
-        TransferHelper.safeApprove(
-            ARB_WETH,
-            address(nonfungiblePositionManager),
-            amount0ToMint
-        );
-        TransferHelper.safeApprove(
-            ARB_USDC,
-            address(nonfungiblePositionManager),
-            amount1ToMint
-        );
+		// set the owner and data for position
+		// operator is msg.sender
+		deposits[tokenId] = Deposit({
+			owner: owner,
+			liquidity: liquidity,
+			token0: token0,
+			token1: token1
+		});
+	}
 
-        (_tokenId, liquidity, amount0, amount1) = nonfungiblePositionManager
-            .mint(
-                INonfungiblePositionManager.MintParams({
-                    token0: ARB_WETH,
-                    token1: ARB_USDC,
-                    fee: poolFee,
-                    // By using TickMath.MIN_TICK and TickMath.MAX_TICK,
-                    // we are providing liquidity across the whole range of the pool.
-                    // Not recommended in production.
-                    tickLower: TickMath.MIN_TICK,
-                    tickUpper: TickMath.MAX_TICK,
-                    amount0Desired: amount0ToMint,
-                    amount1Desired: amount1ToMint,
-                    // NEED TO IMPLEMENT SLIPPAGE PROTECTION
-                    amount0Min: 0,
-                    amount1Min: 0,
-                    recipient: address(this),
-                    deadline: block.timestamp
-                })
-            );
+	function mintNewPosition(
+		uint amount0ToMint,
+		uint amount1ToMint
+	)
+		external
+		onlyOwner
+		returns (
+			uint _tokenId,
+			uint128 liquidity,
+			uint amount0,
+			uint amount1
+		)
+	{
+		// Approve the position manager
+		TransferHelper.safeApprove(
+			ARB_WETH,
+			address(nonfungiblePositionManager),
+			amount0ToMint
+		);
+		TransferHelper.safeApprove(
+			ARB_USDC,
+			address(nonfungiblePositionManager),
+			amount1ToMint
+		);
 
-        // Create a deposit
-        _createDeposit(msg.sender, _tokenId);
+		(
+			_tokenId,
+			liquidity,
+			amount0,
+			amount1
+		) = nonfungiblePositionManager.mint(
+			INonfungiblePositionManager.MintParams({
+				token0: ARB_WETH,
+				token1: ARB_USDC,
+				fee: poolFee,
+				// By using TickMath.MIN_TICK and TickMath.MAX_TICK,
+				// we are providing liquidity across the whole range of the pool.
+				// Not recommended in production.
+				tickLower: TickMath.MIN_TICK,
+				tickUpper: TickMath.MAX_TICK,
+				amount0Desired: amount0ToMint,
+				amount1Desired: amount1ToMint,
+				// NEED TO IMPLEMENT SLIPPAGE PROTECTION
+				amount0Min: 0,
+				amount1Min: 0,
+				recipient: address(this),
+				deadline: block.timestamp
+			})
+		);
 
-        // Remove allowance and refund in both assets.
-        if (amount0 < amount0ToMint) {
-            TransferHelper.safeApprove(
-                ARB_WETH,
-                address(nonfungiblePositionManager),
-                0
-            );
-            // useless to refund owner because we want to keep funds in the same contract ?
-            // uint refund0 = amount0ToMint - amount0;
-            // TransferHelper.safeTransfer(ARB_WETH, msg.sender, refund0);
-        }
+		// Create a deposit
+		_createDeposit(msg.sender, _tokenId);
 
-        if (amount1 < amount1ToMint) {
-            TransferHelper.safeApprove(
-                ARB_USDC,
-                address(nonfungiblePositionManager),
-                0
-            );
-            // useless to refund owner because we want to keep funds in the same contract ?
-            // uint refund1 = amount1ToMint - amount1;
-            // TransferHelper.safeTransfer(ARB_USDC, msg.sender, refund1);
-        }
+		// Remove allowance and refund in both assets.
+		if (amount0 < amount0ToMint) {
+			TransferHelper.safeApprove(
+				ARB_WETH,
+				address(nonfungiblePositionManager),
+				0
+			);
+			// useless to refund owner because we want to keep funds in the same contract ?
+			// uint refund0 = amount0ToMint - amount0;
+			// TransferHelper.safeTransfer(ARB_WETH, msg.sender, refund0);
+		}
 
-        nonfungiblePositionManager.safeTransferFrom(
-            address(this),
-            msg.sender,
-            _tokenId
-        );
+		if (amount1 < amount1ToMint) {
+			TransferHelper.safeApprove(
+				ARB_USDC,
+				address(nonfungiblePositionManager),
+				0
+			);
+			// useless to refund owner because we want to keep funds in the same contract ?
+			// uint refund1 = amount1ToMint - amount1;
+			// TransferHelper.safeTransfer(ARB_USDC, msg.sender, refund1);
+		}
 
-        emit newMint(_tokenId, liquidity, amount0, amount1);
-    }
+		nonfungiblePositionManager.safeTransferFrom(
+			address(this),
+			msg.sender,
+			_tokenId
+		);
 
-    function withdrawLiquidity(
-        uint256 tokenId
-    ) external returns (uint256 amount0, uint256 amount1) {
-        // require is only for testing to not forget about approval, can be removed in production
-        require(
-            nonfungiblePositionManager.isApprovedForAll(
-                msg.sender,
-                address(this)
-            ),
-            "Contract doesn't have ApprovalForAll"
-        );
+		emit newMint(_tokenId, liquidity, amount0, amount1);
+	}
 
-        // caller must be the owner of the NFT
-        require(msg.sender == deposits[tokenId].owner, "Not the owner");
+	function withdrawLiquidity(
+		uint256 tokenId
+	) external returns (uint256 amount0, uint256 amount1) {
+		// require is only for testing to not forget about approval, can be removed in production
+		require(
+			nonfungiblePositionManager.isApprovedForAll(
+				msg.sender,
+				address(this)
+			),
+			"Contract doesn't have ApprovalForAll"
+		);
 
-        // Transfer ownership to itself (smart contract)
-        // Smart contract needs to be setApprovalForAll !!
-        nonfungiblePositionManager.safeTransferFrom(
-            msg.sender,
-            address(this),
-            tokenId
-        );
+		// caller must be the owner of the NFT
+		require(msg.sender == deposits[tokenId].owner, "Not the owner");
 
-        // get liquidity data for tokenId
-        uint128 liquidity = deposits[tokenId].liquidity;
+		// Transfer ownership to itself (smart contract)
+		// Smart contract needs to be setApprovalForAll !!
+		nonfungiblePositionManager.safeTransferFrom(
+			msg.sender,
+			address(this),
+			tokenId
+		);
 
-        // amount0Min and amount1Min are price slippage checks
-        // if the amount received after burning is not greater than these minimums, transaction will fail
+		// get liquidity data for tokenId
+		uint128 liquidity = deposits[tokenId].liquidity;
 
-        // Decrease full liquidity (basically delete position)
-        // RETURN fee0 and fee1
-        nonfungiblePositionManager.decreaseLiquidity(
-            INonfungiblePositionManager.DecreaseLiquidityParams({
-                tokenId: tokenId,
-                liquidity: liquidity,
-                // NEED TO IMPLEMENT SLIPPAGE PROTECTION
-                amount0Min: 0,
-                amount1Min: 0,
-                deadline: block.timestamp
-            })
-        );
+		// amount0Min and amount1Min are price slippage checks
+		// if the amount received after burning is not greater than these minimums, transaction will fail
 
-        // Caller must own the ERC721 position
-        // Call to safeTransfer will trigger `onERC721Received` which must return the selector else transfer will fail
-        // set amount0Max and amount1Max to uint256.max to collect all fees
+		// Decrease full liquidity (basically delete position)
+		// RETURN fee0 and fee1
+		nonfungiblePositionManager.decreaseLiquidity(
+			INonfungiblePositionManager.DecreaseLiquidityParams({
+				tokenId: tokenId,
+				liquidity: liquidity,
+				// NEED TO IMPLEMENT SLIPPAGE PROTECTION
+				amount0Min: 0,
+				amount1Min: 0,
+				deadline: block.timestamp
+			})
+		);
 
-        // Collect fees + tokens owed from decrease liquidity
-        // to get fees earned amount0 - fee0 and amount1 - fee1
-        // WARNING: Need to look about rounding...
-        (amount0, amount1) = nonfungiblePositionManager.collect(
-            INonfungiblePositionManager.CollectParams({
-                tokenId: tokenId,
-                recipient: address(this),
-                amount0Max: type(uint128).max,
-                amount1Max: type(uint128).max
-            })
-        );
+		// Caller must own the ERC721 position
+		// Call to safeTransfer will trigger `onERC721Received` which must return the selector else transfer will fail
+		// set amount0Max and amount1Max to uint256.max to collect all fees
 
-        // Transfer NFT back to owner for safety reasons
-        nonfungiblePositionManager.safeTransferFrom(
-            address(this),
-            msg.sender,
-            tokenId
-        );
+		// Collect fees + tokens owed from decrease liquidity
+		// to get fees earned amount0 - fee0 and amount1 - fee1
+		// WARNING: Need to look about rounding...
+		(amount0, amount1) = nonfungiblePositionManager.collect(
+			INonfungiblePositionManager.CollectParams({
+				tokenId: tokenId,
+				recipient: address(this),
+				amount0Max: type(uint128).max,
+				amount1Max: type(uint128).max
+			})
+		);
 
-        emit collect(amount0, amount1);
-    }
+		// Transfer NFT back to owner for safety reasons
+		nonfungiblePositionManager.safeTransferFrom(
+			address(this),
+			msg.sender,
+			tokenId
+		);
 
-    modifier onlyOwner() {
-        require(msg.sender == i_owner, "Not Owner!");
-        _;
-    }
+		// emit new event collect
+		emit collect(amount0, amount1);
+	}
+
+	function safeWithdraw() external onlyOwner {
+		(uint amount0, uint amount1) = (
+			IERC20(ARB_WETH).balanceOf(address(this)),
+			IERC20(ARB_USDC).balanceOf(address(this))
+		);
+
+		TransferHelper.safeTransfer(ARB_WETH, msg.sender, amount0);
+		TransferHelper.safeTransfer(ARB_USDC, msg.sender, amount1);
+
+		emit withdraw(amount0, amount1);
+	}
+
+	modifier onlyOwner() {
+		require(msg.sender == i_owner, "Not Owner!");
+		_;
+	}
 }
--- test/ranger.test.ts
+++ test/ranger.test.ts
@@ -1,5 +1,5 @@
-import { expect } from 'chai';
-import { ethers, deployments } from 'hardhat';
+import { expect } from "chai";
+import { ethers, deployments } from "hardhat";
 import {
 	Contract,
 	TransactionReceipt,
@@ -8,31 +8,31 @@ import {
 	ContractTransactionReceipt,
 	EventLog,
 	Log,
-} from 'ethers';
-import { IERC20, INonfungiblePositionManager } from '../typechain-types';
-import { HardhatEthersSigner } from '@nomicfoundation/hardhat-ethers/signers';
+} from "ethers";
+import { IERC20, INonfungiblePositionManager } from "../typechain-types";
+import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
 
 const POOL = {
 	ETH_MAINNET: {
-		USDC: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
-		DAI: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+		USDC: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+		DAI: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
 		FEE: 100,
 	},
 	ARBITRUM: {
-		WETH: '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1',
-		USDC: '0xaf88d065e77c8cC2239327C5EDb3A432268e5831',
+		WETH: "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1",
+		USDC: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
 		FEE: 500,
 	},
 };
 
 const WHALE = {
 	ETH_MAINNET: {
-		DAI: '0xFd546293a729fE1A05D249Ad4F2CA984082F889e',
-		USDC: '0x1bf0Aa215DAB195f21372105F53661e46F962ff3',
+		DAI: "0xFd546293a729fE1A05D249Ad4F2CA984082F889e",
+		USDC: "0x1bf0Aa215DAB195f21372105F53661e46F962ff3",
 	},
 	ARBITRUM: {
-		WETH: '0x3368e17064C9BA5D6f1F93C4c678bea00cc78555',
-		USDC: '0xD7a827FBaf38c98E8336C5658E4BcbCD20a4fd2d',
+		WETH: "0x3368e17064C9BA5D6f1F93C4c678bea00cc78555",
+		USDC: "0xD7a827FBaf38c98E8336C5658E4BcbCD20a4fd2d",
 	},
 };
 
@@ -41,8 +41,9 @@ const PARAMS = {
 	token1amount: 250000n * 10n ** 6n,
 };
 
-describe('Ranger', async () => {
+describe("Ranger", async () => {
 	let contract: Contract;
+	let contractAddress: string;
 	let accounts: HardhatEthersSigner[];
 	let token0: IERC20;
 	let token1: IERC20;
@@ -51,19 +52,21 @@ describe('Ranger', async () => {
 	let liquidity: number;
 
 	before(async () => {
-		await deployments.fixture(['all']);
+		await deployments.fixture(["all"]);
 
 		accounts = await ethers.getSigners();
 
-		const tmp3 = await deployments.get('Ranger');
+		const tmp3 = await deployments.get("Ranger");
 		contract = await ethers.getContractAt(tmp3.abi, tmp3.address);
 
+		contractAddress = await contract.getAddress();
+
 		token0 = await ethers.getContractAt(
-			'IERC20',
+			"IERC20",
 			POOL.ARBITRUM.WETH,
 		);
 		token1 = await ethers.getContractAt(
-			'IERC20',
+			"IERC20",
 			POOL.ARBITRUM.USDC,
 		);
 
@@ -89,10 +92,10 @@ describe('Ranger', async () => {
 			.transfer(accounts[0].address, PARAMS.token1amount);
 	});
 
-	it('Pool with given parameters exist', async () => {
+	it("Pool with given parameters exist", async () => {
 		const factory = await ethers.getContractAt(
-			'IUniswapV3Factory',
-			'0x1F98431c8aD98523631AE4a59f267346ea31F984',
+			"IUniswapV3Factory",
+			"0x1F98431c8aD98523631AE4a59f267346ea31F984",
 		);
 
 		const pool = await factory.getPool(
@@ -102,13 +105,11 @@ describe('Ranger', async () => {
 		);
 
 		expect(pool).to.not.equal(
-			'0x0000000000000000000000000000000000000000',
+			"0x0000000000000000000000000000000000000000",
 		);
 	});
 
-	it('Mint new position', async () => {
-		const contractAddress = await contract.getAddress();
-
+	it("Mint new position", async () => {
 		await token0
 			.connect(accounts[0])
 			.transfer(contractAddress, PARAMS.token0amount);
@@ -132,8 +133,8 @@ describe('Ranger', async () => {
 
 		const nfmp: INonfungiblePositionManager =
 			await ethers.getContractAt(
-				'INonfungiblePositionManager',
-				'0xC36442b4a4522E871399CD717aBDD847Ab11FE88',
+				"INonfungiblePositionManager",
+				"0xC36442b4a4522E871399CD717aBDD847Ab11FE88",
 			);
 
 		// VERY IMPORTANT !!!!
@@ -148,7 +149,7 @@ describe('Ranger', async () => {
 		// then set tokenId
 		let _tokenId: string;
 		let _liquidity: number;
-		const event: ContractEvent = contract.getEvent('newMint');
+		const event: ContractEvent = contract.getEvent("newMint");
 		receipt?.logs.forEach((e) => {
 			const t = e as EventLog;
 			if (t.fragment == event.getFragment()) {
@@ -164,12 +165,10 @@ describe('Ranger', async () => {
 		liquidity = _liquidity!;
 	});
 
-	it('Collect all fees and withdraw position', async () => {
-		const contractAddress = await contract.getAddress();
-
+	it("Collect all fees and withdraw position", async () => {
 		const nfmp = await ethers.getContractAt(
-			'INonfungiblePositionManager',
-			'0xC36442b4a4522E871399CD717aBDD847Ab11FE88',
+			"INonfungiblePositionManager",
+			"0xC36442b4a4522E871399CD717aBDD847Ab11FE88",
 		);
 
 		const token0_before_balance =
@@ -182,11 +181,11 @@ describe('Ranger', async () => {
 		const position = await nfmp.positions(tokenId);
 
 		// Liquidity
-		expect(position[7].toString()).to.equal('0');
+		expect(position[7].toString()).to.equal("0");
 
 		// tokensOwed0 and tokensOwed1
-		expect(position[10].toString()).to.equal('0');
-		expect(position[11].toString()).to.equal('0');
+		expect(position[10].toString()).to.equal("0");
+		expect(position[11].toString()).to.equal("0");
 
 		// Check that smart contract received the tokens back
 		const token0_after_balance =
@@ -200,6 +199,54 @@ describe('Ranger', async () => {
 		);
 		expect(token1_after_balance).to.greaterThan(
 			token1_before_balance,
+		);
+	});
+
+	it("Withdraw funds from contract to owner", async () => {
+		const token0_deployer_before_balance = await token0.balanceOf(
+			accounts[0].address,
+		);
+		const token1_deployer_before_balance = await token1.balanceOf(
+			accounts[0].address,
+		);
+
+		// Try to withdraw if not owner, transaction should be reverted
+		const notowner = contract.connect(accounts[1]) as Contract;
+		await expect(notowner.safeWithdraw()).to.be.revertedWith(
+			"Not Owner!",
+		);
+
+		const token0_contract_before_balance =
+			await token0.balanceOf(contractAddress);
+		const token1_contract_before_balance =
+			await token1.balanceOf(contractAddress);
+
+		await contract.safeWithdraw();
+
+		const token0_contract_after_balance =
+			await token0.balanceOf(contractAddress);
+		const token1_contract_after_balance =
+			await token1.balanceOf(contractAddress);
+
+		// Check that contract balance for token0 and token1 is now 0
+		expect(token0_contract_after_balance).to.equal(0);
+		expect(token1_contract_after_balance).to.equal(0);
+
+		const token0_deployer_after_balance = await token0.balanceOf(
+			accounts[0].address,
+		);
+		const token1_deployer_after_balance = await token1.balanceOf(
+			accounts[0].address,
+		);
+
+		// Check that owner has received the tokens
+		expect(token0_deployer_after_balance).to.equal(
+			token0_deployer_before_balance +
+				token0_contract_before_balance,
+		);
+		expect(token1_deployer_after_balance).to.equal(
+			token1_deployer_before_balance +
+				token1_contract_before_balance,
 		);
 	});
 });
